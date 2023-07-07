b49ceb4bcb8945215862735df16dfc5b9a31ce72
genesis-0000 <thaibao@autonomous.nyc>
2023-07-07 11:44:35 +0700

    - init

--- index.js
+++ index.js
@@ -1,26 +1,3 @@
-const rand = mathRand();
-
-// ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓ Add traits here ↓↓↓↓↓↓↓↓↓↓↓↓↓↓
-
-function T1(rand) {
-    // calculate rarity by yourself
-    if (rand > 0.5) {
-        return true;
-    } else {
-        return false
-    }
-}
-
-// window.$generativeTraits contain list traits for token
-// you need to populate a $generativeTraits object in the window object
-window.$generativeTraits = {
-    "T1": T1(rand),
-}
-console.log(window.$generativeTraits)
-
-// ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑ Add traits here ↑↑↑↑↑↑↑↑↑↑↑↑↑↑
-
-
 // ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓ Add your code here ↓↓↓↓↓↓↓↓↓↓↓↓↓↓
 
 const canvas = document.createElement("canvas");
